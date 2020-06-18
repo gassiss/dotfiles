e48439d1826a3209b417bf17944e74ae4821368d
Will <william@williamassis.com>
2020-06-17 22:26:55 -0300

    tweak setup and add wsltty config file

--- setup.sh
+++ setup.sh
@@ -43,24 +43,29 @@ mkdir -p $DOTFILES/zsh/plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $DOTFILES/zsh/plugins/zsh-syntax-highlighting
 
 # Enables gruvbox theme for WSLTTY
-WSLTTY_THEME_PATH="/mnt/c/Users/$WINUSER/AppData/Roaming/wsltty/themes"
-WSLTTY_THEME="$WSLTTY_THEME_PATH/gruvbox-wsltty-theme"
+WSLTTY_CONFIG_PATH="/mnt/c/Users/$WINUSER/AppData/Roaming/wsltty"
+WSLTTY_THEME="$WSLTTY_CONFIG_PATH/themes/gruvbox-wsltty-theme"
 
 if [ ! -f "$WSLTTY_THEME" ]; then
-  mkdir -p $WSLTTY_THEME_PATH
+  mkdir -p "$WSLTTY_THEME_PATH/themes"
   cp $DOTFILES/gruvbox-wsltty-theme $WSLTTY_THEME
 fi
+
+# WSLTTY config
+cp $HOME/.dotfiles/wslttyconfig $WSLTTY_CONFIG_PATH/config
 
 # Enabling gitmux
 curl -o $HOME/gitmux.tar.gz -L https://github.com/arl/gitmux/releases/download/v0.6.0/gitmux_0.6.0_linux_amd64.tar.gz
 tar -xvzf $HOME/gitmux.tar.gz
 sudo mv $HOME/gitmux /usr/bin/gitmux
+rm $HOME/gitmux.tar.gz
 
 # Makes symbolic links to dotfiles
 mkdir -p $HOME/.config/nvim
 ln -s $DOTFILES/nvim/coc-settings.json $HOME/.config/nvim/coc-settings.json
 ln -s $DOTFILES/tmux/.tmux.conf $HOME/.tmux.conf
 ln -s $DOTFILES/zsh/.zshrc $HOME/.zshrc
+ln -s $DOTFILES/nvim/init.vim $HOME/.config/nvim/init.vim
 
 # Make zsh default shell
 chsh -s "$(which zsh)"
