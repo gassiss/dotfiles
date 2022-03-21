153d9ec9ae512540e4f1c26c6d3307c48316e52d
Will Assis <william@williamassis.com>
2022-03-20 21:15:11 -0300

    add gd shortcut

--- nvim/.config/nvim/lua/gassiss/lsp.lua
+++ nvim/.config/nvim/lua/gassiss/lsp.lua
@@ -1,4 +1,9 @@
 -- npm install -g typescript typescript-language-server
-require("lspconfig").tsserver.setup{}
+require("lspconfig").tsserver.setup{
+  on_attach = function ()
+    vim.api.nvim_buf_set_keymap(0, "n", "gd", ":lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+    -- Nnoremap("gd", ":lua vim.lsp.buf.definition()<CR>")
+  end
+}
 -- npm i -g vscode-langservers-extracted
 require("lspconfig").eslint.setup{}
