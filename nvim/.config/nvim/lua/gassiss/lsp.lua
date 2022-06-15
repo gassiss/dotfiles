999ba625ed983481c71eca8bc98bba72f2f833e1
Will Assis <william@williamassis.com>
2022-06-15 08:33:52 -0300

    more config

--- nvim/.config/nvim/lua/gassiss/lsp.lua
+++ nvim/.config/nvim/lua/gassiss/lsp.lua
@@ -2,6 +2,14 @@
 require("lspconfig").tsserver.setup{
   on_attach = function ()
     vim.api.nvim_buf_set_keymap(0, "n", "gd", ":lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, "n", "gt", ":lua vim.lsp.buf.type_definition()<CR>", { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, "n", "grr", ":lua vim.lsp.buf.references()<CR>", { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, "n", "grn", ":lua vim.lsp.buf.rename()<CR>", { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, "n", "K", ":lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, "n", "[d", ":lua vim.diagnostic.goto_next()<CR>", { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, "n", "]d", ":lua vim.diagnostic.goto_prev()<CR>", { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, "i", "<C-h>", ":lua vim.lsp.buf.signature_help()<CR>", { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, "n", "<leader>vd", ":lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
     -- Nnoremap("gd", ":lua vim.lsp.buf.definition()<CR>")
   end
 }
