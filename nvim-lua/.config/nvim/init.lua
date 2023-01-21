8904fdfc755b0ca017969bd2ba0c79167a5a6b43
Will Assis <william@williamassis.com>
2023-01-21 15:42:15 -0500

    binds

--- nvim-lua/.config/nvim/init.lua
+++ nvim-lua/.config/nvim/init.lua
@@ -3,18 +3,13 @@ require("config.mappings")
 require("config.autocmd")
 require("config.lazy")
 
--- missing from old config:
--- dap
---
 -- todo:
 -- revamp some keybinds
 -- * ensure most used (current) ones are either control + key or leader + key
--- * telescope: grep (and find reference if possible) from current dir
 -- * learn more about snippets (long effort)
 -- * setup dap (long effort)
 -- interesting plugins to maybe use:
 -- * leap -> allows to reach places with less keystrokes
--- * RRethy/vim-illuminate -> highlights symbols like vscode
 -- * folke/trouble.nvim -> cool diagnostics window
 -- * nvim-neo-tree/neo-tree.nvim -> replace netwr maybe
 -- * vim-surround or mini.surround for easy add/delete '"` tags etc
