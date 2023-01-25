aae11044a9b288327919cc542b13eda8fbc8ad87
Will Assis <william@williamassis.com>
2023-01-25 13:54:47 -0500

    mv nvim-lua to nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,5 +6,4 @@ require("config.lazy")
 -- todo:
 -- revamp some keybinds
 -- * ensure most used (current) ones are either control + key or leader + key
--- * learn more about snippets (long effort)
 -- * setup dap (long effort)
