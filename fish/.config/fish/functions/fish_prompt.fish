51021474a8ad7b50a9d98da88a70720c67b79c2c
Will Assis <william@williamassis.com>
2024-03-02 11:25:06 -0300

    remove hydro code for minimal prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,6 +1,21 @@
-function fish_prompt --description "Stolen from jorgebucaran/hydro"
-    echo -e "$_hydro_color_pwd$_hydro_pwd$hydro_color_normal $_hydro_color_git$$_hydro_git$hydro_color_normal$_hydro_color_duration$_hydro_status$hydro_color_normal "
-end
+function fish_prompt --description 'Write out the prompt'
+    set -l last_pipestatus $pipestatus
+    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+    set -l normal (set_color normal)
+    set -q fish_color_status
+    or set -g fish_color_status red
 
-set -gx hydro_color_pwd green
-set -gx hydro_color_git cyan
+    # Write pipestatus
+    # If the status was carried over (if no command is issued or if `set` leaves the status untouched), don't bold it.
+    set -l bold_flag --bold
+    set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
+    if test $__fish_prompt_status_generation = $status_generation
+        set bold_flag
+    end
+    set __fish_prompt_status_generation $status_generation
+    set -l status_color (set_color $fish_color_status)
+    set -l statusb_color (set_color $bold_flag $fish_color_status)
+    set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+
+    echo -n -s (set_color $fish_color_cwd) (prompt_pwd) (set_color cyan) (fish_vcs_prompt) $normal " "$prompt_status "\$ "
+end
--- fish/.config/fish/functions/fish_right_prompt.fish
+++ fish/.config/fish/functions/fish_right_prompt.fish
@@ -1,5 +1,5 @@
 function fish_right_prompt --description "Right prompt"
-  echo -e "$_kubectl_color$_rprompt_kubectl$hydro_color_normal $_rprompt_user_host"
+  echo -e "$_kubectl_color$_rprompt_kubectl$normal $_rprompt_user_host"
 end
 
 set -g _kubectl_color (set_color purple)
